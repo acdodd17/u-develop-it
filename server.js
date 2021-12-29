33435d26fce14696f317baf08c1ea37887bb76e4
Anna <acdodd17@gmail.com>
2021-12-29 09:27:53 -0500

    connected mysql database to server

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
+//import express 
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+//import mysql
+const mysql = require('mysql2');
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost', 
+        user: 'root', 
+        password:'MySQLacdr@tt3rr331713!', 
+        database: 'election'
+    }, 
+    console.log('Connected to the election database.')
+);
+
 //Express middleware
 app.use(express.urlencoded({ extend: false }));
 app.use(express.json());
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
