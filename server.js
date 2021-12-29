9f3d0b2900a73cfac3d80474ee763f06b0dd3953
Anna <acdodd17@gmail.com>
2021-12-29 15:24:30 -0500

    voter GET routes

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-//const inputCheck = require('./utils/inputCheck');
 const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
 
